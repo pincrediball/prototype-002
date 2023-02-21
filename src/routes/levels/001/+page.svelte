2513cc33e1c968b722fad5bac1c83e2bf9d43165
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-02-21 14:55:27 +0100

    Tweaked button

--- src/routes/levels/001/+page.svelte
+++ src/routes/levels/001/+page.svelte
@@ -17,16 +17,18 @@
   }
 </script>
 
-<div class="px-4 py-1">
-  <div class="bg-white border-4 border-black shadow-firm inline-block">
-    <div id="game" class="w-[400px] h-[600px]" />
+<div class="p-4 flex flex-col">
+  <div>
+    <div class="bg-white border-4 border-black shadow-firm inline-block">
+      <div id="game" class="w-[400px] h-[600px]" />
+    </div>
   </div>
-</div>
-<div class="px-4 py-1">
-  <button
-    class="px-4 py-2 bg-amber-300 text-neutral-900 font-bold hover:bg-amber-500"
-    on:click={plunge}
-  >
-    Plunge!
-  </button>
+  <div class="w-[400px] flex justify-end">
+    <button
+      class="px-4 py-2 bg-amber-300 text-neutral-900 font-bold hover:bg-amber-500"
+      on:click={plunge}
+    >
+      Plunge!
+    </button>
+  </div>
 </div>
